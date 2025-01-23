82e5387fba2bda17cea86fb9814096620482b2f5
choi_gwangcheol <46204876+cleanlight93@users.noreply.github.com>
2025-01-23 18:30:06 +0900

    Update Head.tsx

--- source/quartz/components/Head.tsx
+++ source/quartz/components/Head.tsx
@@ -54,7 +54,7 @@ export default (() => {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-XXXXXXXXXX');
+              gtag('config', 'G-ZBQ0EBDE1J');
             `,
           }}
         />
