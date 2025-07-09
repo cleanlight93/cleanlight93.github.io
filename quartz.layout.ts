dcade08f5e4ce6b2fdd0f2ad22a51d7d25cdfe50
choi_gwangcheol <46204876+cleanlight93@users.noreply.github.com>
2025-07-09 18:43:18 +0900

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,5 +1,8 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
+import { QuartzPluginData } from "./quartz/plugins/vfile"
+import { FullSlug, SimpleSlug } from "./quartz/util/path"
+import { getDate } from "./quartz/components/Date"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
@@ -65,4 +68,37 @@ export const defaultListPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [],
+}
+
+// components for the home page (index.md)
+export const homePageLayout: Partial<FullPageLayout> = {
+  beforeBody: [
+    Component.ArticleTitle(),
+    Component.ContentMeta(),
+  ],
+  left: [
+    Component.PageTitle(),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Search(),
+    Component.Darkmode(),
+    Component.Explorer(),
+  ],
+  right: [
+    Component.Graph({
+      localGraph: {
+        depth: -1,  // 모든 페이지 보여줌
+
+      }
+    }),
+    Component.DesktopOnly(Component.TableOfContents()),
+  ],
+  afterBody: [
+    Component.RecentNotes({
+      title: "최근 글",
+      limit: 15,
+      showTags: true,
+      linkToMore: false,
+      filter: (f: QuartzPluginData) => f.slug !== "index" // index 페이지 제외
+    }),
+  ],
 }
