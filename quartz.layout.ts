4d81aebe48c2e7686ea79020c1007da409824788
choi_gwangcheol <46204876+cleanlight93@users.noreply.github.com>
2025-07-09 22:12:26 +0900

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -11,8 +11,9 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/",
+      "INFOMAX": "https://news.einfomax.co.kr/",
+      "BOK" : "https://www.bok.or.kr/portal/main/main.do",
     },
   }),
 }
