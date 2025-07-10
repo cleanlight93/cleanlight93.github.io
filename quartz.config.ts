87e68d17348df7d69e0b206e62eb0800c2aad274
choi_gwangcheol <46204876+cleanlight93@users.noreply.github.com>
2025-07-10 09:27:34 +0900

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,9 +24,9 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "Noto Sans",
+        body: "Noto Sans",
+        code: "Consolas",
       },
       colors: {
         lightMode: {
