450e08f4379f981c43c2ad738d60423c15f60b12
choi_gwangcheol <46204876+cleanlight93@users.noreply.github.com>
2025-08-24 15:44:58 +0900

    Update latex.ts

--- quartz/plugins/transformers/latex.ts
+++ quartz/plugins/transformers/latex.ts
@@ -47,7 +47,7 @@ export const Latex: QuartzTransformerPlugin<Options> = (opts?: Options) => {
               {
                 /** MathJax v3 SVG 어댑터(옵션) – 사이트 크기 최적화 시 유용 */
                 src: "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js",
-                loadTime: "afterDOMReady",
+                loadTime: "beforeDOMReady",
                 contentType: "external",
               },
             ],
