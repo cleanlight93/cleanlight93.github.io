5e5aecad030756c4ea2e5b96e287a22558e538c6
choi_gwangcheol <46204876+cleanlight93@users.noreply.github.com>
2025-08-24 16:06:28 +0900

    Update latex.ts

--- quartz/plugins/transformers/latex.ts
+++ quartz/plugins/transformers/latex.ts
@@ -50,7 +50,7 @@ export const Latex: QuartzTransformerPlugin<Options> = (opts?: Options) => {
       if (engine === "katex") {
         return [[rehypeKatex, { output: "html" }]]
       } else {
-        return [rehypeMathjax]
+        return [[rehypeMathjax, {}]] 
       }
     },
   }
